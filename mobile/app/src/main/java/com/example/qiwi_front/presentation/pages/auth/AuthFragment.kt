34c1663b4510387cd8efc8803f3cb5c98bcc5f4a
KamilMustafin <mustafin@hightech.group>
2022-11-12 01:42:19 +0300

    добавил инъекцию

--- mobile/app/src/main/java/com/example/qiwi_front/presentation/pages/auth/AuthFragment.kt
+++ mobile/app/src/main/java/com/example/qiwi_front/presentation/pages/auth/AuthFragment.kt
@@ -3,8 +3,12 @@ package com.example.qiwi_front.presentation.pages.auth
 import com.example.qiwi_front.base.fragment.FragmentBase
 import com.example.qiwi_front.databinding.AuthFragmentBinding
 import com.example.qiwi_front.databinding.StatesBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class AuthFragment : FragmentBase<AuthFragmentBinding, AuthViewModel>() {
+@AndroidEntryPoint
+class AuthFragment @Inject constructor() : FragmentBase<AuthFragmentBinding, AuthViewModel>() {
+
     override fun getViewModelClass(): Class<AuthViewModel> = AuthViewModel::class.java
 
     override fun getViewBinding(): AuthFragmentBinding = AuthFragmentBinding.inflate(layoutInflater)
