81735b67e0cd5fd3833a7321feeee13f82909831
KamilMustafin <mustafin@hightech.group>
2022-11-12 00:34:14 +0300

    добавил модули и все библотеки

---
+++ mobile/data/src/main/java/com/example/data/core/ktorClient/KtorClient.kt
@@ -0,0 +1,65 @@
+package com.example.data.core.ktorClient
+
+import android.util.Log
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import java.nio.charset.Charset
+
+private const val TIME_OUT = 60_000
+
+open class KtorClient() : KtorClientBase() {
+
+    init {
+        ktorHttpClient = HttpClient(Android) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    ignoreUnknownKeys = true
+                    coerceInputValues = true
+                })
+
+                engine {
+                    connectTimeout = TIME_OUT
+                    socketTimeout = TIME_OUT
+                }
+            }
+
+            install(Logging) {
+                level = LogLevel.BODY
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.v("Logger Ktor =>", message)
+                    }
+
+                }
+            }
+
+            defaultRequest {
+                header(HttpHeaders.ContentType, ContentType.Application.Json)
+                header(HttpHeaders.ContentType, Charset.forName("UTF-8"))
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = ""
+                }
+            }
+
+            expectSuccess = true
+            HttpResponseValidator {
+                handleResponseExceptionWithRequest { cause, request ->
+                    when (cause) {
+                        // ToDo обработка ошибок с серва
+                    }
+                }
+            }
+
+        }
+    }
+
+}
---
+++ mobile/data/src/main/java/com/example/data/core/ktorClient/KtorClientBase.kt
@@ -0,0 +1,109 @@
+package com.example.data.core.ktorClient
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
+
+open class KtorClientBase() {
+    init {
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts: Array<TrustManager> = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun getAcceptedIssuers(): Array<X509Certificate>? {
+                return null
+            }
+
+            override fun checkClientTrusted(certs: Array<X509Certificate?>?, authType: String?) {}
+            override fun checkServerTrusted(certs: Array<X509Certificate?>?, authType: String?) {}
+        })
+
+        try {
+            // Install the all-trusting trust manager
+            val sc: SSLContext =
+                SSLContext.getInstance("SSL")
+
+            sc.init(null, trustAllCerts, SecureRandom())
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
+        }
+        catch (ex: Exception) {}
+
+        // Create all-trusting host name verifier
+        val allHostsValid: HostnameVerifier = object : HostnameVerifier {
+            override fun verify(hostname: String?, session: SSLSession?): Boolean {
+                return true
+            }
+        }
+
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid)
+    }
+
+    companion object {
+        lateinit var ktorHttpClient: HttpClient
+    }
+
+    suspend inline fun <reified T> get(
+        url: String,
+    ): T? {
+        var response: T? = null
+
+        try {
+            val httpResponse: HttpResponse = ktorHttpClient.get(url)
+            response = httpResponse.body()
+        } catch (ex: Exception) {
+        }
+        return response
+    }
+
+    suspend inline fun <reified T> getWithParameters(
+        url: String,
+        list: List<Pair<String, String>>,
+    ): T? {
+        var response: T? = null
+
+        try {
+            val httpResponse = ktorHttpClient.get(url) {
+                list.forEach {
+                    parameter(it.first, it.second)
+                }
+            }
+            response = httpResponse.body()
+        } catch (ex: Exception) {
+        }
+
+        return response
+    }
+
+    suspend fun post(
+        urlRequest: String, requestBody: Any,
+    ) {
+
+        try {
+            ktorHttpClient.post(urlRequest) {
+                setBody(requestBody)
+            }
+        } catch (ex: Exception) {
+        }
+    }
+
+    suspend inline fun <reified T> postWithResponse(
+        urlRequest: String,
+        requestBody: Any,
+    ): T? {
+        var response: T? = null
+
+
+        try {
+            val httpResponse = ktorHttpClient.post(urlRequest) {
+                setBody(requestBody)
+            }
+            response = httpResponse.body()
+        } catch (ex: Exception) {
+        }
+        return response
+    }
+}
