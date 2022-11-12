2d21fe928a073f589fb21eb41793a771f2de32e7
Niyaz Sabitov <n.sabitov@hightech.group>
2022-11-12 23:41:30 +0300

    JWT

---
+++ backend/Infrastructure/JWT/UserRoles.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.JWT;
+
+public static class UserRoles
+{
+    public const string Seller = "Seller";
+    public const string User = "User";
+}
---
+++ backend/Logic/Auth/AuthenticateController.cs
@@ -0,0 +1,84 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Context.Models;
+using Logic.Auth.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Logic.Auth;
+
+[Route("[controller]")]
+[ApiController]
+public class AuthenticateController : ControllerBase
+{
+    private readonly UserManager<User> _userManager;
+    private readonly RoleManager<Role> _roleManager;
+    private readonly IConfiguration _configuration;
+
+    public AuthenticateController(
+        UserManager<User> userManager,
+        RoleManager<Role> roleManager,
+        IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _configuration = configuration;
+    }
+    
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto model)
+    {
+        var user = await _userManager.Users
+            .FirstOrDefaultAsync(x =>x.PhoneNumber == model.PhoneNumber);
+
+        var foo = _userManager.PasswordHasher
+            .VerifyHashedPassword(user, user.PasswordHash, model.Password);
+        if (user != null && 
+            _userManager.PasswordHasher
+                .VerifyHashedPassword(user, user.PasswordHash ,model.Password) 
+            == PasswordVerificationResult.Success)
+        {
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+            var token = GetToken(authClaims);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            });
+        }
+        return Unauthorized();
+    }
+    
+    private JwtSecurityToken GetToken(List<Claim> authClaims)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidAudience"],
+            expires: DateTime.Now.AddHours(3),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return token;
+    }
+}
