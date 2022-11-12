020054bfb601348dd5b586ccde63e5647bfe52f4
Данил Архипов <danilarkhipov00@gmail.com>
2022-11-13 01:48:42 +0300

    Правка ревью

---
+++ backend/Context/Migrations/20221112222808_expand_seller_model.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class expandsellermodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Sellers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Caregory",
+                table: "Sellers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Sellers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Sellers");
+
+            migrationBuilder.DropColumn(
+                name: "Caregory",
+                table: "Sellers");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Sellers");
+        }
+    }
+}
