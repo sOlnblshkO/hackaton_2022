876a447d6a2101291e53b5645cd7e5ad47ddfaa6
Данил Архипов <danilarkhipov00@gmail.com>
2022-11-12 04:10:55 +0300

    Добавил нижнее меню навигации

---
+++ mobile/app/src/main/java/com/example/qiwi_front/presentation/pages/scanner/ScannerViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.qiwi_front.presentation.pages.scanner
+
+import com.example.qiwi_front.base.viewModel.ViewModelBase
+import javax.inject.Inject
+
+class ScannerViewModel @Inject constructor() : ViewModelBase(){
+}
